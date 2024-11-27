f53b8be251f29ee42d8b4ac4f2b24c90b419246d
Mariana Gabriely <marianagabiely@edu.unifil.br>
2024-11-27 14:38:45 -0300

    Adiciona função simples de soma

---
+++ main.py
@@ -0,0 +1,11 @@
+def imprimir_mensagem():
+    print("Olá")
+
+def soma(a, b):
+    return a + b
+
+print("Hello, World!")
+imprimir_mensagem()
+
+resultado = soma(3, 5)
+print(f"A soma é: {resultado}")
